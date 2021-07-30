17bac1a0751d40cfca8ce34e0edc7cc2c48e3da4
Discord Bot Tutorials <75545991+DiscordBotTutorials@users.noreply.github.com>
2021-07-30 10:50:52 -0700

    Update suggestions.js

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -4,7 +4,7 @@ module.exports = {
     permissions: [],
     description: 'creates a suggestion!',
     execute(message, args, cmd, client, discord){
-        const channel = message.guild.channels.cache.find(c => c.name === '❓・suggestions');
+        const channel = message.guild.channels.cache.find(c => c.name === 'suggestions');
         if(!channel) return message.channel.send('suggestions channel does not exist!');
 
         let messageArgs = args.join(' ');
