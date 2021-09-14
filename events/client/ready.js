f10fa580a771f3f133230bb58f75aed5e6822057
Discord Bot Tutorials <75545991+DiscordBotTutorials@users.noreply.github.com>
2021-09-13 19:15:53 -0500

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -2,7 +2,7 @@ module.exports = (Discord, client) => {
     console.log('CodeLyon Bot is Online!');
 
 
-    client.user.setActivity('Credit goes to Discord Bot Tutorials.', { type: 'PLAYING' })
+    client.user.setActivity('Credit goes to CodeLyon.', { type: 'PLAYING' })
     .then(presence => console.log(`Activity set to ${presence.activities[0].name}`))
     .catch(console.error);
 }
